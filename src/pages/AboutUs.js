6c5c3012cd1c22d5a16aace13c8d3691842c08bf
foster <foster.ogwudu@yahoo.com>
2022-10-12 01:30:55 +0100

    updated background as image

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -10,11 +10,12 @@ import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sig
 import Csr from "components/csr/TwoColumnForCSR.js";
 import { ReactComponent as ArrowUp } from "feather-icons/dist/icons/arrow-up.svg";
 import { ReactComponent as Heart } from "feather-icons/dist/icons/heart.svg";
+import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
 
 import Truck2 from "../images/Truck-1-on-white-background.png";
 import Origin from "components/hero/Origin";
 import Footer from "components/footers/Footer";
-import ImageBackground from "components/hero/ImageBackground";
+
 
 const Subheading = tw.span`uppercase tracking-wider text-sm`;
 const HighlightedText = tw.span`text-primary-500`;
@@ -41,89 +42,86 @@ export default () => {
   });
   return (
     <AnimationRevealPage>
-      <ImageBackground showWhite={showWhite}>
-        <StyledDiv>
-          <button
-            className={
-              show
-                ? "rotateInDownLeft scroll d-flex justify-content-center align-items-center"
-                : "d-none"
-            }
-            onClick={() => {
-              window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-            }}
-            style={{
-              backgroundColor: "#F15A29",
-            }}
-          >
-            <ArrowUp />
-          </button>
+      <StyledDiv>
+        <Hero showWhite={showWhite}/>
+        <button
+          className={
+            show
+              ? "rotateInDownLeft scroll d-flex justify-content-center align-items-center"
+              : "d-none"
+          }
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+          }}
+          style={{
+            backgroundColor: "#F15A29",
+          }}
+        >
+          <ArrowUp />
+        </button>
 
-          <a
-            href="https://wa.me/2347061146501"
-            class="whatsapp_float bounceIn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i class="fa fa-whatsapp whatsapp-icon"></i>
-          </a>
-          <Origin subheading={<Subheading>About Us</Subheading>} linkText="" />
+        <a
+          href="https://wa.me/2347061146501"
+          class="whatsapp_float bounceIn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fa fa-whatsapp whatsapp-icon"></i>
+        </a>
+        <Origin subheading={<Subheading>About Us</Subheading>} linkText="" />
 
-          <MainFeature2
-            subheading={
-              <Subheading className="brandPrimary">VALUES</Subheading>
-            }
-            primaryButtonText="Our Services"
-            primaryButtonUrl="/services"
-            description="Our objective is to be the most transparent, standard, and safest brand in Nigeria. We are committed to making our customers happy and relaxed by simplifying logistics and making truck procurement as affordable as possible by assisting you in buying directly from European market. "
-            heading={
-              <>
-                We Live By Our <HighlightedText>Principles.</HighlightedText>
-              </>
-            }
-            imageSrc={Truck2}
-            showDecoratorBlob={false}
-            features={[
-              {
-                Icon: Star,
-                title: "Standard",
-                description:
-                  "There is no NemFra without standard. Excellence is our second nature. With our standard trucks, technology and well trained professionals, you are sure of getting nothing but the best.",
-                iconContainerCss: tw`bg-green-300 text-green-800`,
-              },
-              {
-                Icon: Heart,
-                title: "Safety",
-                description:
-                  "Health and Safety is part of our corporate culture. It is not something that has to be done, it is something that is accepted as a responsibility that ensures safety of our drivers and customer goods.",
-                iconContainerCss: tw`bg-red-300 text-red-800`,
-              },
-              {
-                Icon: MoneyIcon,
-                title: "Transpearency",
-                description:
-                  "All our services are rendered with honesty and sincerity without any hidden charges or obscure process. We promise to offer you the best logistic rate we can - at par with the industry standard. As for purchasing trucks, we make sure you buy the best at European price. We are simply open book.",
-                iconContainerCss: tw`bg-orange-300 text-orange-800`,
-              },
-            ]}
-          />
+        <MainFeature2
+          subheading={<Subheading className="brandPrimary">VALUES</Subheading>}
+          primaryButtonText="Our Services"
+          primaryButtonUrl="/services"
+          description="Our objective is to be the most transparent, standard, and safest brand in Nigeria. We are committed to making our customers happy and relaxed by simplifying logistics and making truck procurement as affordable as possible by assisting you in buying directly from European market. "
+          heading={
+            <>
+              We Live By Our <HighlightedText>Principles.</HighlightedText>
+            </>
+          }
+          imageSrc={Truck2}
+          showDecoratorBlob={false}
+          features={[
+            {
+              Icon: Star,
+              title: "Standard",
+              description:
+                "There is no NemFra without standard. Excellence is our second nature. With our standard trucks, technology and well trained professionals, you are sure of getting nothing but the best.",
+              iconContainerCss: tw`bg-green-300 text-green-800`,
+            },
+            {
+              Icon: Heart,
+              title: "Safety",
+              description:
+                "Health and Safety is part of our corporate culture. It is not something that has to be done, it is something that is accepted as a responsibility that ensures safety of our drivers and customer goods.",
+              iconContainerCss: tw`bg-red-300 text-red-800`,
+            },
+            {
+              Icon: MoneyIcon,
+              title: "Transpearency",
+              description:
+                "All our services are rendered with honesty and sincerity without any hidden charges or obscure process. We promise to offer you the best logistic rate we can - at par with the industry standard. As for purchasing trucks, we make sure you buy the best at European price. We are simply open book.",
+              iconContainerCss: tw`bg-orange-300 text-orange-800`,
+            },
+          ]}
+        />
 
-          <Csr
-            subheading={
-              <Subheading className="brandPrimary">
-                Corporate Social Responsibility
-              </Subheading>
-            }
-            heading={
-              <>
-                Franca Ogwudu <HighlightedText>Foundation</HighlightedText>
-              </>
-            }
-            description='As our name was coined from "Nne m Franca" - "My Mother Franca", we partner with Franca Ogwudu foundation in rendering help in our communities through education scholarships and charities to the less priviledged'
-          />
-          <Footer />
-        </StyledDiv>
-      </ImageBackground>
+        <Csr
+          subheading={
+            <Subheading className="brandPrimary">
+              Corporate Social Responsibility
+            </Subheading>
+          }
+          heading={
+            <>
+              Franca Ogwudu <HighlightedText>Foundation</HighlightedText>
+            </>
+          }
+          description='As our name was coined from "Nne m Franca" - "My Mother Franca", we partner with Franca Ogwudu foundation in rendering help in our communities through education scholarships and charities to the less priviledged'
+        />
+        <Footer />
+      </StyledDiv>
     </AnimationRevealPage>
   );
 };
