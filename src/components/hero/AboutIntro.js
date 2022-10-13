ab2aa16ba547b233e8386e95f6af371cb3369c19
foster <foster.ogwudu@yahoo.com>
2022-10-13 17:20:16 +0100

    changed animation in about us

--- src/components/hero/AboutIntro.js
+++ src/components/hero/AboutIntro.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import tw from "twin.macro";
 
@@ -6,8 +5,8 @@ const Paragraph = tw.h2`font-bold text-3xl md:text-4xl lg:text-4xl xl:text-4xl`;
 const AboutIntro = () => {
   return (
     <div className="mt-5">
-      <span className="text-center curve mt-5">
-        <Paragraph className="innerCurve mt-5">
+      <span className="text-center curve mt-5 ">
+        <Paragraph className="innerCurve mt-5 rotateInDownLeft">
           What Makes Us{" "}
           <strong className="brandPrimary">
             Nem<span className="brandSecondary">Fra</span>
@@ -27,9 +26,8 @@ const AboutIntro = () => {
             <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z" />
           </svg>
           <p className="quote">
-            Our clients and partners are  family to us. Hence we
-            render our services passionately with that Motherly care and
-            transparency.
+            Our clients and partners are family to us. Hence we render our
+            services passionately with that Motherly care and transparency.
           </p>
           <p className="author">
             —{" "}
